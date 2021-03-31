67a10595de3a45de460c69c09d7a52da97fcf905
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-03-31 19:49:05 -0400

    added profile picture and series/films links to browse

--- netflix-clone2/src/components/header/index.js
+++ netflix-clone2/src/components/header/index.js
@@ -8,6 +8,11 @@ import {
   Feature,
   FeatureCallOut,
   Text,
+  Link,
+  Group,
+  Picture,
+  Profile,
+  Dropdown,
 } from "./styles/header";
 
 export default function Header({ bg = true, children, ...restProps }) {
@@ -25,12 +30,32 @@ Header.FeatureCallOut = function HeaderFeatureCallOut({
   return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
 };
 
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+  return <Profile {...restProps}>{children}</Profile>;
+};
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+  return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+};
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
+};
+
 Header.Text = function HeaderText({ children, ...restProps }) {
   return <Text {...restProps}>{children}</Text>;
 };
 
+Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>;
+};
+
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
+};
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>;
 };
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
