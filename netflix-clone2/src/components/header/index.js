cef4f530762df0578bd8ec27fb74971235ec40a6
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-03-31 20:34:35 -0400

    added searh bar

--- netflix-clone2/src/components/header/index.js
+++ netflix-clone2/src/components/header/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link as ReactRouterLink } from "react-router-dom";
 import {
   Background,
@@ -13,6 +13,9 @@ import {
   Picture,
   Profile,
   Dropdown,
+  Search,
+  SearchInput,
+  SearchIcon,
 } from "./styles/header";
 
 export default function Header({ bg = true, children, ...restProps }) {
@@ -36,6 +39,30 @@ Header.Profile = function HeaderProfile({ children, ...restProps }) {
 
 Header.Picture = function HeaderPicture({ src, ...restProps }) {
   return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+};
+
+Header.Search = function HeaderSearch({
+  searchTerm,
+  setSearchTerm,
+  ...restProps
+}) {
+  const [searchActive, setSearchActive] = useState(false);
+
+  return (
+    <Search {...restProps}>
+      <SearchIcon
+        onClick={() => setSearchActive((searchActive) => !searchActive)}
+      >
+        <img src="/images/icons/search.png" alt="Search" />
+      </SearchIcon>
+      <SearchInput
+        value={searchTerm}
+        onChange={({ target }) => setSearchTerm(target.value)}
+        placeholder="Search films and series"
+        active={searchActive}
+      />
+    </Search>
+  );
 };
 
 Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
